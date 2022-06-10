bf6da3f103191056f38f7cd0e3ea3208b721fbf5
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-06-10 11:12:05 -0300

    added clearInputs()

--- script.js
+++ script.js
@@ -4,23 +4,33 @@ cep.addEventListener("focusout", searchCep =  async() =>{
     const value = cep.value
     const url = `https://viacep.com.br/ws/${value}/json`
 
-        const search = await fetch(url).catch(error => {
-            if(value != ""){           
-                 document.querySelector(".message").style.display = "flex"
+    const search = await fetch(url).catch(error => {
+        if(value != ""){           
+                document.querySelector(".message").style.display = "flex"
 
-                 cep.value = ""
-            }
-        })
+            clearInputs()
+        }
+    })
 
-        const  information = await search.json()
+    const  information = await search.json()
 
-        let city = document.querySelector("#city").value = information.localidade
-        let adress = document.querySelector("#adress").value = information.logradouro
-        let district = document.querySelector("#district").value = information.bairro
-        let state = document.querySelector("#state").value = information.uf
+    let city = document.querySelector("#city").value = information.localidade
+    let adress = document.querySelector("#adress").value = information.logradouro
+    let district = document.querySelector("#district").value = information.bairro
+    let state = document.querySelector("#state").value = information.uf
 
 })
 
 function hide(){
     document.querySelector(".message").style.display = "none"
+}
+
+function clearInputs(){
+    
+    document.querySelector("#cep").value = ""
+    document.querySelector("#city").value = ""
+    document.querySelector("#adress").value = ""
+    document.querySelector("#district").value = ""
+    document.querySelector("#state").value = ""
+
 }
