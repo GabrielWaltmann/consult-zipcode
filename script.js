edc3685a414724cc33229c5a505c3601ea34336d
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-06-09 23:40:42 -0300

    added searchCep function

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 cep = document.querySelector("#cep")
 
-cep.addEventListener("blur",(e)=>{
-    let search = cep.value.replace("-", "")
+cep.addEventListener("focusout", searchCep =  async() =>{
+    const value = cep.value
+    const url = `https://viacep.com.br/ws/${value}/json`
 
-    const options = {
-        method: "get",
-        mode: "cors",
-        cache: "default"
-    }
+        const search = await fetch(url).catch(error => {
+            if(value != ""){           
+                 document.querySelector(".message").style.display = "flex"
 
-    fetch(`https://viacep.com.br/ws/${search}
-    /json`, options)
-    .then(Response=>{Response.json().then()})
-    
-    console.log()
+                 cep.value = ""
+            }
+        })
+
+        const  information = await search.json()
+
+        let city = document.querySelector("#city").value = information.localidade
+        let adress = document.querySelector("#adress").value = information.logradouro
+        let district = document.querySelector("#district").value = information.bairro
+        let state = document.querySelector("#state").value = information.uf
+
 })
+
+function hide(){
+    document.querySelector(".message").style.display = "none"
+}
